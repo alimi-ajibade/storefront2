369025f8b0a973da0f78c81d845a52a028256644
HelloSolo <davidalimi148@gmail.com>
2022-05-26 17:55:11 +0100

    Added api view for products

--- store/views.py
+++ store/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+# Prevents the repitition of the try/except block for 404
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
+
+
+@api_view()  # Replaces django request object with that of the rest_framework
+def product_list(request):
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def product_details(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
