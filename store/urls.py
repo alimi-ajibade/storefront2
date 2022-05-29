1feba027ecf15e0d8879ffcbbc75cdf29dbfc72f
HelloSolo <davidalimi148@gmail.com>
2022-05-29 14:18:06 +0100

    Switched to routers

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>/', views.product_details),
-    path('collections/', views.collection_list),
-    path(
-        'collections/<int:id>/',
-        views.collection_detail,
-        name='collection-detail',
-    ),
-]
+
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:pk>/', views.ProductDetails.as_view()),
+#     path('collections/', views.CollectionList.as_view()),
+#     path(
+#         'collections/<int:pk>/',
+#          views.CollectionDetail.as_view(),
+#          name='collection-detail',
+#     ),
+# ]
