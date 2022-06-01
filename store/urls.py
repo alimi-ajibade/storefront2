3f6ed6c77558062e5ed3d2652e1e5c7281684781
HelloSolo <davidalimi148@gmail.com>
2022-06-01 19:04:10 +0100

    Used Nested Links for Products and Review

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,22 @@
+from cgitb import lookup
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+
+products_router.register('reviews', views.ReviewViewset,
+                         basename='product-reviews')
+
+print(products_router.urls)
+urlpatterns = router.urls + products_router.urls
 
 
 # urlpatterns = [
