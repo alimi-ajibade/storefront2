8f6c3b9b675eab82064590c29fca572533599cfe
HelloSolo <davidalimi148@gmail.com>
2022-06-01 19:03:44 +0100

    Added Serializer for Review

--- store/serializers.py
+++ store/serializers.py
@@ -7,7 +7,11 @@ from decimal import Decimal
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['id', 'date', 'name', 'description', 'product']
+        fields = ['id', 'date', 'name', 'description']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)
 
 
 class CollectionSerializer(serializers.ModelSerializer):
