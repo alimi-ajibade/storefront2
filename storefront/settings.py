18e80f200b4483cbc435b034075768b5b540a80c
HelloSolo <davidalimi148@gmail.com>
2022-06-03 13:37:20 +0100

    Setup djoser

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'playground',
     'debug_toolbar',
     'store',
@@ -145,6 +146,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Prevent rest from changing unit price to a string
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
 }
 
 AUTH_USER_MODEL = 'core.User'
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer'
+    }
+}
