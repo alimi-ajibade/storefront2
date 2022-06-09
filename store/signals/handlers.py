cdf495b0450aa000f318001693086af882a0de6f
HelloSolo <davidalimi148@gmail.com>
2022-06-09 17:23:51 +0100

    Added handlers

---
+++ store/signals/handlers.py
@@ -0,0 +1,13 @@
+
+from django.conf import settings
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from store.models import Customer
+
+
+# Anytime a user instance is created, a customer instance is generated automatically
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_customer_for_new_user(sender, **kwargs):
+    if kwargs['created']:
+        Customer.objects.create(user=kwargs['instance'])
