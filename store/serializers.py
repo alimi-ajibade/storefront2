bba35c6869f93c8fc6216b97018cf7416453e903
HelloSolo <davidalimi148@gmail.com>
2022-06-09 17:21:34 +0100

    Added Order and OrderItem Serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,8 @@
-from dataclasses import fields
-from itertools import product
-from logging import error
+from dataclasses import field
+from django.db import transaction
 from rest_framework import serializers
-from store.models import Cart, CartItem, Customer, Product, Collection, Review
+from store.models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
+from .signals import order_created
 from decimal import Decimal
 
 
@@ -135,3 +135,67 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'unit_price', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'placed_at',
+                  'payment_status', 'items']
+
+
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def validate_cart_id(self, cart_id):
+        if not Cart.objects.filter(pk=cart_id).exists():
+            raise serializers.ValidationError(
+                'No cart with the given ID was found')
+        if CartItem.objects.filter(cart_id=cart_id).count() == 0:
+            raise serializers.ValidationError('The Cart is empty')
+        return cart_id
+
+    # Transactions are used to keep the database consistent
+    # by make sure that a set of actions are performed and
+    # if an error occurs the effects of the previous operations
+    # are undone, the set of insttructions can only be performed as a whole
+    with transaction.atomic():
+        def save(self, **kwargs):
+            cart_id = self.validated_data['cart_id']
+            customer = Customer.objects.get(
+                user_id=self.context['user_id'])
+            order = Order.objects.create(customer=customer)
+
+            cart_items = CartItem.objects.select_related(
+                'product').filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    unit_price=item.product.unit_price,
+                    quantity=item.quantity
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+
+            Cart.objects.filter(pk=cart_id).delete()
+
+            order_created.send_robust(self.__class__, order=order)
+
+            return order
+
+
+class UpdateOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['payment_status']
