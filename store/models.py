84e9e347ece7c705d8ca66cae07efb6eb5bf6bef
HelloSolo <davidalimi148@gmail.com>
2022-06-13 09:13:35 +0100

    Added Product Images

--- store/models.py
+++ store/models.py
@@ -1,9 +1,9 @@
-from distutils.command.upload import upload
 from django.contrib import admin
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
+from store.validators import validate_file_size
 
 
 class Promotion(models.Model):
@@ -48,7 +48,8 @@ class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
     # The route used here is relative to the Media directory
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(upload_to='store/images',
+                              validators=[validate_file_size])
 
 
 class Customer(models.Model):
